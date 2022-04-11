3c715af9b237ac89852ffb788d612754d4132886
MaksymkinPylyp <94921828+MaksymkinPylyp@users.noreply.github.com>
2022-04-11 21:10:51 +0300

    Game over

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 const dino = document.getElementById ('dino');
 const cactus = document.getElementById ('cactus');
+const gameWindow = document.querySelector ('.game');
+const gameOverPerent = document.querySelector ('.gameOver');
+const refreshBtn = document.querySelector ('.refresh');
+
+
 
 document.addEventListener ('keydown',function(event) {
     jump();
+    console.log('score');
 });
+
+function refreshPage(){
+    window.location.reload();
+} 
 
 function jump () {
     if (dino.classList != 'jump') {
@@ -14,11 +24,23 @@ function jump () {
     }, 300);
 }
 
+function gameOver () {
+    gameWindow.classList.add('hide');
+    gameWindow.classList.remove('show');
+    refreshBtn.classList.add('button');
+    refreshBtn.classList.remove('hide');
+
+    gameOverPerent.innerHTML = `
+    <img src="img/SeekPng.com_game-over-png_2391119.png" alt="game over" class="img">
+    `;
+}
+
 let isAlive = setInterval(function() {
     let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue('top'));
     let cactusLeft = parseInt(window.getComputedStyle(cactus ).getPropertyValue('left'));
 
     if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
-        alert('Game over');
+        console.log('Game over');
+        gameOver();
     }
 }, 10);
